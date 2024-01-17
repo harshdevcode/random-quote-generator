72ed19c5b26c9f7a9c3cc5ed0d90e3ddac34703f
harshmo <harsh.raj@xecurify.com>
2024-01-17 17:13:14 +0530

    Random Quote generator code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react'
+import axios from "axios";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+
+  const [advice,setAdvice]=useState("");
+
+  useEffect(() => {
+    fetchAdvice();
+  }, []);
+
+  const fetchAdvice = () =>{
+    axios
+          .get("https://api.adviceslip.com/advice")
+          .then((response) => {
+              const { advice } = response.data.slip;
+
+              setAdvice(advice);
+          })
+
+          .catch((error) => {
+              console.log(error);
+          });
 }
 
-export default App;
+  return (
+    <div className="app">
+                <div className="card">
+                    <h1 className="heading">{advice}</h1>
+                    <button className="button" onClick={fetchAdvice
+                    }>
+                        <span>Give Me Advice</span>
+                    </button>
+                </div>
+            </div>
+  )
+}
+
+export default App
